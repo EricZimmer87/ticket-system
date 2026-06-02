476873c77ddfc5a73915b0571581b9e3ba8bf77c
EricZimmer87 <github@ejzimmer.com>
2026-06-02 13:11:35 -0500

    2026-06-02 1311

---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketSystem.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
