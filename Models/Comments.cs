8b79c12fe7d718ce542da04c4ac2f349f9bc0bde
EricZimmer87 <github@ejzimmer.com>
2026-06-02 19:49:45 -0500

    2026-06-02 1949

---
+++ Models/Comments.cs
@@ -0,0 +1,15 @@
+﻿namespace TicketSystem.Models
+{
+    public class Comments
+    {
+        public int CommentId { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+        public int TicketId { get; set; }
+        public Tickets Ticket { get; set; } = null!;
+
+        public int UserId { get; set; }
+        public Users User { get; set; } = null!;
+    }
+}
---
+++ Models/Tickets.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketSystem.Models
+{
+    public class Tickets
+    {
+        public int TicketId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Status { get; set; } = "Open";
+    }
+}
