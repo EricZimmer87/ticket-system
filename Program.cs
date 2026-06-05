f10a3385d8353261bdc49e326811025cc1a29da2
Eric Zimmer <ericzimmer87@fastmail.com>
2026-06-05 11:19:51 -0500

    2026-06-05 1119

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TicketSystem.Data;
 
@@ -9,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Swagger
+builder.Services.AddSwaggerGen();
+
 // Database connection string
 var connectionString =
     builder.Configuration.GetConnectionString("DefaultConnection")
@@ -19,17 +23,34 @@ var connectionString =
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+// Register the Identity DbContext with the dependency injection container
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+   options.UseSqlServer(connectionString));
+
+// Add authorization services to dependency injection container
+builder.Services.AddAuthorization();
+
+// Activate Identity APIs
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Explicitly add authentication to the middleware pipeline
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapIdentityApi<IdentityUser>();
 
 app.MapControllers();
 
